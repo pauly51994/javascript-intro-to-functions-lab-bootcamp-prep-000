c8c798b14b8c1d025e1b9de195ffb9e89dab9c91
Paul Yun <p.yun1994@gmail.com>
2019-06-21 19:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ function sayHiToGrandma(string){
   var lowercase = 'hello';
   if (string === lowercase){return "I can't hear you!"}
   else if (string === uppercase){return "YES INDEED!"}
-  else if (string === "I love you, Grandma"){return "I love you, too."};
+  else if (string === "I love you, Grandma."){return "I love you, too."}
+  else {return "something is wrong"};
 }
